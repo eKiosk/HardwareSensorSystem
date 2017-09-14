9d12b6531a99144d6a2c5dd6647c7bf2503bdfe4
NicatorBa <daniel@bartschinski.info>
2017-09-14 21:39:23 +0200

    Add project for authentication and authorization

---
+++ src/HardwareSensorSystem.Security/SecurityBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class SecurityBuilderExtensions
+    {
+        public static IApplicationBuilder UseSecurity(this IApplicationBuilder app)
+        {
+            app.UseAuthentication();
+            return app;
+        }
+    }
+}
---
+++ src/HardwareSensorSystem.Security/SecurityServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class SecurityServiceCollectionExtensions
+    {
+        public static void AddSecuriy(this IServiceCollection services)
+        {
+        }
+    }
+}
